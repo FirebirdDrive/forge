bb7118685631daf889797e97b5f8c0558e0ed89f
Alvaro Flores <aflores2016579@kinal.edu.gt>
2021-09-20 10:55:30 -0600

    Test buttons
    test buttons

--- public/js/ForgeTree.js
+++ public/js/ForgeTree.js
@@ -181,3 +181,15 @@ function translateObject(node) {
     },
   });
 }
+// used by viewer.addEventListener(Autodesk.Viewing.TOOLBAR_CREATED_EVENT, onToolbarCreated)
+const onToolbarCreated = (e) => {
+  const settingsTools = viewer.toolbar.getControl('settingsTools')
+  settingsTools.removeControl('toolbar-modelStructureTool')
+  settingsTools.removeControl('toolbar-propertiesTool')
+  settingsTools.removeControl('toolbar-settingsTool')
+  settingsTools.removeControl('toolbar-fullscreenTool')
+
+  viewer.removeEventListener(
+    Autodesk.Viewing.TOOLBAR_CREATED_EVENT,
+    onToolbarCreated)
+}
