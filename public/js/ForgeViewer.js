724c78f75f35080a05d1377357ac65700b2a584a
Alvaro Flores <aflores2016579@kinal.edu.gt>
2021-09-23 16:11:01 -0600

    Test css buttons

--- public/js/ForgeViewer.js
+++ public/js/ForgeViewer.js
@@ -50,3 +50,15 @@ function getForgeToken(callback) {
     });
   });
 }
+
+const onToolbarCreated = (e) => {
+  const settingsTools = viewer.toolbar.getControl('settingsTools')
+  settingsTools.removeControl('toolbar-modelStructureTool')
+  settingsTools.removeControl('toolbar-propertiesTool')
+  settingsTools.removeControl('toolbar-settingsTool')
+  settingsTools.removeControl('toolbar-fullscreenTool')
+
+  viewer.removeEventListener(
+    Autodesk.Viewing.TOOLBAR_CREATED_EVENT,
+    onToolbarCreated)
+}
